04392c1f6faa3d330991fb587fe2dcf52898de66
Mesfin Bekele Mekonnen <mesfinmekonnen@MacBook-Pro.local>
2017-02-22 01:27:11 -0500

    move FFRaceable protocol to its own file and use protocol RaceTrack file

--- FastAndFurious/FFRacecar.h
+++ FastAndFurious/FFRacecar.h
@@ -1,14 +1,8 @@
 #import <Foundation/Foundation.h>
-
-@protocol FFRaceable
-
-@property (nonatomic, readonly) float topSpeed;
-@property (nonatomic, readonly) float durability;
-@property (nonatomic, readonly) NSString *racecarID;
-
-@end
+#import "FFRaceable.h"
 
 @interface FFRacecar : NSObject <FFRaceable>
+
 
 - (instancetype)initWithTopSpeed:(float)topSpeed
                       durability:(float)durablility;
